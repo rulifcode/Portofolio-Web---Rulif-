164ad11dce2e56944fe4adaf8a3d07c8f96fee8b
Rulif Fadria Nirwansyah <ruliffax@gmail.com>
2026-04-27 23:23:26 +0700

    add gaotek

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -16,7 +16,7 @@ import { useLang } from "../components/layout/Navbar";
 
 import logoVodjo     from "../assets/vodjo.webp";
 import logoLumoshive from "../assets/lumoshive.png";
-import logoGaotek    from "../assets/gaotek.png";
+import logoGaotek    from "../assets/GAOTek.png.png";
 
 const PROJECTS_TRANSLATIONS = {
   EN: {
